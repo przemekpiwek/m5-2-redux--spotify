7395ecd2ebb978a71949c9629794143cb6282567
Przemek Piwek <przemekpiwek@gmail.com>
2020-09-03 10:32:45 -0400

    Exercise 2 complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+div,
+span {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+}
+
+/* GLOBAL STYLES */
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  font-family: Montserrat, sans-serif;
+}
+`;
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <>
+      <GlobalStyle />
+      <Router>TODO</Router>
+    </>
+  );
 };
 
 export default App;
